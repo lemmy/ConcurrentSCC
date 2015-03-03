8c9e848aff0f862301bd5a5ff9d921bd995a57fe
Markus Alexander Kuppe <markus@kuppe.org>
2015-03-03 15:21:39 +0100

    Remove unused equals and hashCode

--- src/org/kuppe/graphs/tarjan/GraphNode.java
+++ src/org/kuppe/graphs/tarjan/GraphNode.java
@@ -128,31 +128,9 @@ public class GraphNode implements Comparable<GraphNode> {
 		return contracted;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GraphNode other = (GraphNode) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	public int compareTo(GraphNode o) {
 		return id.compareTo(o.id);
 	}
