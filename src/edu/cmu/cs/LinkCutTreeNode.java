ff3e554705fc85077eb59ce9b37b0ea59467c242
Markus Alexander Kuppe <markus@kuppe.org>
2015-06-30 13:21:56 +0200

    Remvoe hashCode and equals

--- src/edu/cmu/cs/LinkCutTreeNode.java
+++ src/edu/cmu/cs/LinkCutTreeNode.java
@@ -23,47 +23,8 @@ public class LinkCutTreeNode {
 		return preferred == null || (preferred.left != this && preferred.right != this);
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		if (id == -1) {
-			return super.toString();
-		}
-		return "LinkCutTreeNode [id=" + id + "]";
-	}
-
 	public void addChild(LinkCutTreeNode p) {
 		assert p != this;
 		children.add(p);
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		LinkCutTreeNode other = (LinkCutTreeNode) obj;
-		if (id != other.id)
-			return false;
-		return true;
 	}
 }
