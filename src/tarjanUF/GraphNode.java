f4f6a1baba47fd5aa868e4ec58dffd643c4603a1
Parv Mor <parvmor.pm@gmail.com>
2018-08-07 14:09:08 +0530

    Add comments to graph implementation.

--- src/tarjanUF/GraphNode.java
+++ src/tarjanUF/GraphNode.java
@@ -8,11 +8,15 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class GraphNode implements Serializable {
 
+    // List of adjacent nodes that be reached via an outgoing edge from this node.
     private List<Integer> arcs;
+    // Each node is associated with a unique id.
     private int id;
 
+    // Constructor.
     public GraphNode(int id) {
         this.id = id;
+        // Create a dynamically resizable adjacency list.
         arcs = new ArrayList<Integer>();
     }
 
@@ -24,6 +28,7 @@ public class GraphNode implements Serializable {
         return this.arcs;
     }
 
+    // Check if it is a terminal node.
     public boolean hasArcs() {
         if (arcs == null) {
             return false;
@@ -31,10 +36,12 @@ public class GraphNode implements Serializable {
         return !this.arcs.isEmpty();
     }
 
+    // Get the unique id associated with this node.
     public int getId() {
         return this.id;
     }
 
+    // POST returns an iterator over the adjacent nodes of this node.
     public Iterator<Integer> POST() {
         assert this.arcs != null;
         return this.arcs.iterator();
